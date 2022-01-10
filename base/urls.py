77d688882d7a42780a57d01f7dd4d2d82fa8a9f3
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2022-01-10 19:32:18 +0530

    add authentication

--- base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('', views.getRoutes,name="routes"),
-    path('products', views.getProducts,name="getproducts"),
-    path('product/<str:pk>/', views.getProduct,name="getproduct"),
-]
--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,10 +1,12 @@
-from django.db.models import manager
-from django.shortcuts import render 
-from rest_framework.decorators import api_view
+from rest_framework import serializers 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .product import products
-from .models import Product
-from .serializer import ProductSerializer
+from base.product import products
+from base.models import Product, User
+from base.serializer import ProductSerializer
+
+from rest_framework import status
 
 
 @api_view(["GET"])
@@ -12,6 +14,8 @@ def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products,many=True)
     return Response(serializer.data)
+
+
 
 @api_view(["GET"])
 def getProduct(request, pk):
